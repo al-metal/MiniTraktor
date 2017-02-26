782823a18323e5b989fd0ea9f4d717ccb3a65312
al-metal <al-metal@bk.ru>
2017-02-26 11:14:54 +0400

    Получение части данных карточки товара

--- MiniTraktor/Form1.cs
+++ MiniTraktor/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bike18;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,7 @@ namespace MiniTraktor
     public partial class Form1 : Form
     {
         WebRequest webRequest = new WebRequest();
+        nethouse nethouse = new nethouse();
 
         public Form1()
         {
@@ -49,8 +51,66 @@ namespace MiniTraktor
                 {
                     string urlSubCategories = subStr.ToString();
                     urlSubCategories = urlSubCategories.Replace("\">", "").Replace("<a href=\"", "").Trim();
+
+                    GetArrayTovar(urlSubCategories);
                 }
             }
+        }
+
+        private void GetArrayTovar(string urlSubCategories)
+        {
+            string otv = null;
+            otv = webRequest.getRequest(urlSubCategories);
+            MatchCollection tovars = new Regex("(?<=<a class=\"product-loop-title\" href=\")[\\w\\W]*?(?=\"><h3>)").Matches(otv);
+            foreach(Match str in tovars)
+            {
+                string url = str.ToString();
+                GetListTovar(url);
+            }
+        }
+
+        private void GetListTovar(string url)
+        {
+            string otv = null;
+            string name = null;
+            string article = null;
+            string price = null;
+            string category = null;
+            string miniText = null;
+            string fullText = null;
+            string title= null;
+            string description = null;
+            string keywords = null;
+            otv = webRequest.getRequest(url);
+            name = new Regex("(?<=product_title\">).*?(?=</h1>)").Match(otv).ToString();
+            article = new Regex("(?<=itemprop=\"sku\">).*?(?=</span>)").Match(otv).ToString();
+            price = new Regex("(?<=\"price\" content=\").*?(?=\" />)").Match(otv).ToString();
+            category = ReturnCategoryTovar(otv);
+            miniText = ReturnDescriptionText(otv);
+        }
+
+        private string ReturnDescriptionText(string otv)
+        {
+            string description = null;
+            description = new Regex("(?<=class=\"description\">)[\\w\\W]*?(?=</div>)").Match(otv).ToString();
+            MatchCollection urls = new Regex("<a.*?\">").Matches(description);
+            foreach(Match str in urls)
+            {
+                string s = str.ToString();
+                description = description.Replace(s, "").Replace("</a>", "").Trim();
+            }
+            description = nethouse.specChar(description);
+            
+            return description;
+        }
+
+        private string ReturnCategoryTovar(string otv)
+        {
+            string category = "Запчасти и расходники => Запчасти для сельхозтехники и навесного оборудования => ";
+            string strCategory = new Regex("(?<=<div class=\"breadcrumbs\">)[\\w\\W]*?(?=</div>)").Match(otv).ToString();
+            MatchCollection arrayCategory = new Regex("(?<=\">).*?(?=</a>)").Matches(strCategory);
+            category += arrayCategory[2].ToString() + " => " + arrayCategory[3].ToString();
+            return category;
         }
     }
 }
